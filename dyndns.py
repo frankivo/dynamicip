c874304b4e290ae4ce02add3aca0cc9275f4cd1e
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-07-08 23:39:16 +0200

    sleep after

--- dyndns.py
+++ dyndns.py
@@ -6,18 +6,23 @@ import time
 
 def main():
     SLEEP_TIME_SECONDS = int(os.getenv("SLEEP_TIME_SECONDS", "3600")) # Default: 1 hour
-    dns = transip(private_key=os.environ["KEY"], domain=os.environ["DOMAIN"], record=os.environ["DOMAIN_RECORD"], login=os.environ["LOGIN"])
+    dns = transip(
+        private_key = os.environ["KEY"],
+        domain = os.environ["DOMAIN"],
+        record = os.environ["DOMAIN_RECORD"],
+        login = os.environ["LOGIN"]
+    )
 
     my_ip = "0.0.0.0"
 
     while True:
-        time.sleep(SLEEP_TIME_SECONDS)
-
         current_ip = ip.get_ip()
         if not current_ip == my_ip:
             print(f"new ip: {current_ip}")
             my_ip = current_ip
             dns.patch(my_ip)
+
+        time.sleep(SLEEP_TIME_SECONDS)
 
 
 if __name__ == '__main__':
