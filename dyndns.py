8d09d07598d4328f17b81d22fe4ca37b8c7f89a1
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-11-02 23:42:39 +0100

    move

--- dyndns.py
+++ dyndns.py
@@ -6,7 +6,12 @@ import time
 
 def main():
     SLEEP_TIME_SECONDS = int(os.getenv("SLEEP_TIME_SECONDS", "3600")) # Default: 1 hour
-
+    dns = transip(
+        private_key = os.environ["KEY"],
+        domain = os.environ["DOMAIN"],
+        record = os.environ["DOMAIN_RECORD"],
+        login = os.environ["LOGIN"]
+    )
     my_ipv4, my_ipv6 = ("127.0.0.1", "::1")
 
     while True:
@@ -14,15 +19,6 @@ def main():
         current_ip6 = util.get_wan_ip6()
 
         if not (current_ip4 == my_ipv4 or current_ip6 == my_ipv6):
-            dns = transip(
-                private_key = os.environ["KEY"],
-                domain = os.environ["DOMAIN"],
-                record = os.environ["DOMAIN_RECORD"],
-                login = os.environ["LOGIN"]
-            )
-
-            print(f"new ip4: {current_ip4}")
-            print(f"new ip6: {current_ip6}")
             my_ipv4 = current_ip4
             my_ipv6 = current_ip6
 
